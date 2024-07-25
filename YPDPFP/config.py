8dd5f30c18b6ab163f0ee4daff0f754153edc14f
GorgeousTV <demaocean@yandex.ru>
2024-07-25 21:30:22 +0300

    a

--- YPDPFP/config.py
+++
@@ -1,7 +0,0 @@
-# Волков Дмитрий, 19-ая когорта - Финальный проект. Инженер по тестированию плюс
-
-URL_SERVICE = "https://8cc82646-8846-4718-829e-31890832736a.serverhub.praktikum-services.ru"
-DOC_PATH = "/docs/"
-CREATE_COUR_PATH = "/api/v1/courier"
-CREATE_ORDER_PATH = "/api/v1/orders"
-ORDER_LIST = "/v1/orders/track?t="
