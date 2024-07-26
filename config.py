af73f5ffd50d699f2429a0b6b310ed0fc337d2ea
GorgeousTV <demaocean@yandex.ru>
2024-07-26 13:20:44 +0300

    a

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Волков Дмитрий, 19-ая когорта - Финальный проект. Инженер по тестированию плюс
 
-URL_SERVICE = "https://8cc82646-8846-4718-829e-31890832736a.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://f0a5e7aa-021d-4c81-b88d-b52bb7aeb343.serverhub.praktikum-services.ru"
 DOC_PATH = "/docs/"
 CREATE_COUR_PATH = "/api/v1/courier"
 CREATE_ORDER_PATH = "/api/v1/orders"
